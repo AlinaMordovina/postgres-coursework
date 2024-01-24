5fd77a3bd82eeef288d79c56e9d8691f01f60419
Alina Mordovina <alina182@yandex.ru>
2024-01-24 05:34:23 +0400

    update main.py and db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -5,26 +5,75 @@ class DBManager:
     """Класс для работы с данными, полученными из базы данных."""
 
     def __init__(self, database_name: str, params: dict):
-        self.db_name = database_name
-        self.params = params
+        self.conn = psycopg2.connect(dbname=database_name, **params)
+        self.conn.autocommit = True
+
+    def __del__(self):
+        self.conn.close()
 
     def get_companies_and_vacancies_count(self) -> list:
         """Получает список всех компаний и количество вакансий у каждой компании."""
-        pass
+
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                    SELECT employers.name, COUNT(vacancies.vacancy_id) as count_vacancies FROM employers
+                    JOIN vacancies USING(employer_id)
+                    GROUP BY employers.name
+                    ORDER BY count_vacancies
+                """)
+            data = cur.fetchall()
+
+        return data
 
     def get_all_vacancies(self) -> list:
         """Получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию."""
-        pass
 
-    def get_avg_salary(self) -> int:
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                    SELECT employers.name, vacancies.name, vacancies.salary_from, vacancies.salary_to, vacancies.url FROM employers
+                    JOIN vacancies USING(employer_id)
+                    ORDER BY vacancies.salary_from, vacancies.salary_to
+                """)
+            data = cur.fetchall()
+
+        return data
+
+    def get_avg_salary(self) -> float:
         """Получает среднюю зарплату по вакансиям."""
-        pass
+
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                    SELECT AVG(salary_from) as avg_salary from vacancies
+                """)
+            data = cur.fetchone()[0]
+
+        return data
 
     def get_vacancies_with_higher_salary(self) -> list:
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
-        pass
 
-    def get_vacancies_with_keyword(self, words: list) -> list:
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                    SELECT employers.name, vacancies.name, vacancies.salary_from, vacancies.url FROM vacancies
+                    JOIN employers USING(employer_id)
+                    WHERE vacancies.salary_from > (SELECT AVG(salary_from) from vacancies)
+                    ORDER BY vacancies.salary_from DESC
+                """)
+            data = cur.fetchall()
+
+        return data
+
+    def get_vacancies_with_keyword(self, word: str) -> list:
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова."""
-        pass
+
+        with self.conn.cursor() as cur:
+            cur.execute(f"""
+                    SELECT employers.name, vacancies.name, vacancies.salary_from, vacancies.url FROM vacancies
+                    JOIN employers USING(employer_id)
+                    WHERE vacancies.name LIKE '%{word}%'
+                    ORDER BY vacancies.salary_from
+                """)
+            data = cur.fetchall()
+
+        return data
--- main.py
+++ main.py
@@ -4,32 +4,93 @@ from config import config
 
 
 def main():
+    """Функция для взаимодействия с пользователем."""
+
     database_name = 'hh_data'
     params = config()
     employers_ids = [
-        '1740', # Яндекс
-        '1122462', # Skyeng
-        '1541784', # STM
-        '1375441', # Okko
-        '3529', # Сбер
-        '78638', # Тинек
-        '2059150', # Айти-услуги
-        '1755416', # Appbooster
-        '36486', # Magenta
-        '668019' # СимбирСофт
+        '1740',
+        '1122462',
+        '1541784',
+        '1375441',
+        '3529',
+        '78638',
+        '2059150',
+        '1755416',
+        '36486',
+        '668019'
     ]
-    words = []
 
     data = get_hh_data(employers_ids)
     create_database(database_name, params)
     save_data_to_database(data, database_name, params)
-
     data_manager = DBManager(database_name, params)
-    data_manager.get_companies_and_vacancies_count()
-    data_manager.get_all_vacancies()
-    data_manager.get_avg_salary()
-    data_manager.get_vacancies_with_higher_salary()
-    data_manager.get_vacancies_with_keyword(words)
+
+    print("Добрый день!")
+
+    while True:
+        operation_number = input("Выберите номер операции для просмотра информации:\n"
+                                 "- Список всех компаний и количество вакансий у каждой компании: 1\n"
+                                 "- Список всех вакансий: 2\n"
+                                 "- Средняя зарплата по вакансиям: 3\n"
+                                 "- Список всех вакансий, у которых зарплата выше средней по всем вакансиям: 4\n"
+                                 "- Список всех вакансий, в названии которых содержатся переданное слово: 5\n"
+                                 "Для остановки просмотра информации введите слово - стоп\n"
+                                 )
+        if operation_number in ['1', '2', '3', '4', '5', 'стоп']:
+
+            if operation_number == '1':
+                companies = data_manager.get_companies_and_vacancies_count()
+                for company in companies:
+                    print(f"{company[0]} - {company[1]}")
+            elif operation_number == '2':
+                vacancies = data_manager.get_all_vacancies()
+                for vacancy in vacancies:
+                    if vacancy[2] is None and vacancy[3] is None:
+                        print(f"{vacancy[0]}: {vacancy[1]}\n"
+                              f"зарплата: не указана\n"
+                              f"ссылка: {vacancy[4]}")
+                    elif vacancy[2] is not None and vacancy[3] is None:
+                        print(f"{vacancy[0]}: {vacancy[1]}\n"
+                              f"зарплата: от {vacancy[2]}\n"
+                              f"ссылка: {vacancy[4]}")
+                    elif vacancy[2] is None and vacancy[3] is not None:
+                        print(f"{vacancy[0]}: {vacancy[1]}\n"
+                              f"зарплата: до {vacancy[3]}\n"
+                              f"ссылка: {vacancy[4]}")
+                    else:
+                        print(f"{vacancy[0]}: {vacancy[1]}\n"
+                              f"зарплата: от {vacancy[2]} до {vacancy[3]}\n"
+                              f"ссылка: {vacancy[4]}")
+            elif operation_number == '3':
+                avg_salary = data_manager.get_avg_salary()
+                print(round(avg_salary))
+            elif operation_number == '4':
+                big_salary_vacancies = data_manager.get_vacancies_with_higher_salary()
+                for vacancy in big_salary_vacancies:
+                    print(f"{vacancy[0]}: {vacancy[1]}\n"
+                          f"зарплата: от {vacancy[2]}\n"
+                          f"ссылка: {vacancy[3]}")
+            elif operation_number == '5':
+                word = input("Введите слово для поиска:\n")
+                vacancies_with_keyword = data_manager.get_vacancies_with_keyword(word)
+                for vacancy in vacancies_with_keyword:
+                    if vacancy[2] is not None:
+                        print(f"{vacancy[0]}: {vacancy[1]}\n"
+                              f"зарплата: от {vacancy[2]}\n"
+                              f"ссылка: {vacancy[3]}")
+                    else:
+                        print(f"{vacancy[0]}: {vacancy[1]}\n"
+                              f"зарплата: не указана\n"
+                              f"ссылка: {vacancy[3]}")
+
+            elif operation_number.lower() == 'стоп':
+                del data_manager
+                print("Хорошего дня!")
+                break
+
+        else:
+            print("Операции с указанным номером не существует.")
 
 
 if __name__ == '__main__':
